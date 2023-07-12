fcd9386d9a1515752fc4d43ca670fa1abd4859b6
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-07-12 22:36:36 +0300

    Initial Commit

---
+++ iPassword/iPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPasswordApp.swift
+//  iPassword
+//
+//  Created by MikhaiL on 12.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
